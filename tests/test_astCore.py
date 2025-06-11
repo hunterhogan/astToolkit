8e7e6d073ffb4807fa49514b943ba2470fb28d09
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-06-11 15:11:35 -0500

    crappy tests

--- tests/test_astCore.py
+++
@@ -1,5 +0,0 @@
-class TestASTCore:
-    """Tests for core AST functionality and node behavior.
-    Don't make tests for `ast` in `astToolkit`: the `ast` folks can test their own stuff.
-    """
-    pass
