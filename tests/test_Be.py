4f10239047596bec0487331ace9778d0947aa2c0
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-07-02 13:20:23 -0500

    fix: documentation, mutable arguments, tests\dataSamples

--- tests/test_Be.py
+++ tests/test_Be.py
@@ -7,9 +7,9 @@ def test_BeIdentifierClassPositive(beTestData: tuple[str, str, dict[str, Any]]) 
     beMethod = getattr(Be, identifierClass)
     assert beMethod(node), f"Be.{identifierClass} should return True for {subtestName}"
 
-def test_BeIdentifierClassNegative(beNegativeTestData: tuple[str, str, str, dict[str, Any]]) -> None:
-    identifierClass, identifier_vsClass, subtestName, dictionaryTestData = beNegativeTestData
-    node = dictionaryTestData['expression']
-    beMethod = getattr(Be, identifierClass)
-    assert not beMethod(node), f"Be.{identifierClass} should return False for {identifier_vsClass} node in {subtestName}"
+# def test_BeIdentifierClassNegative(beNegativeTestData: tuple[str, str, str, dict[str, Any]]) -> None:
+#     identifierClass, identifier_vsClass, subtestName, dictionaryTestData = beNegativeTestData
+#     node = dictionaryTestData['expression']
+#     beMethod = getattr(Be, identifierClass)
+#     assert not beMethod(node), f"Be.{identifierClass} should return False for {identifier_vsClass} node in {subtestName}"
 
