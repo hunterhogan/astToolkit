9e9cad7123af2a5419a6ae34b28abdbf86ba943d
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-07-15 01:52:29 -0500

    Python 3.14.

--- astToolkit/_prototypeFind.py
+++ astToolkit/_prototypeFind.py
@@ -1,14 +1,25 @@
-# ruff: noqa: A001
 from astToolkit import ConstantValueType
-from collections.abc import Sequence
+from collections.abc import Callable, Sequence
 from typing import Any
 from typing_extensions import TypeIs
 import ast
+import dataclasses
+
+@dataclasses.dataclass
+class Findonaut:
+    attrChain: list[str] = dataclasses.field(default_factory=list[str])
+    FindInstance: list[Callable] = dataclasses.field(default_factory=list[Callable])
+
+    def __post_init__(self) -> None:
+        self.attrActive = None
 
 class Find:
 
-    @classmethod
-    def at(cls, getable: Any, index: int, /) -> object:
+    def __init__(self, state: Findonaut | None=None) -> None:
+        self.state = state or Findonaut()
+
+    @staticmethod
+    def at(getable: Any, index: int, /) -> object:
         return getable.__getitem__(index)
 
     @staticmethod
